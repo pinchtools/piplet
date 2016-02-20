9e9a16398b41eaabda260d45a13c1b4fa7671cd2
vincentp_wm <vincentp@webwag.com>
2016-02-20 15:59:18 +0100

    move logged_in_user to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   include Users::SessionsHelper
+
+  private
+  
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      store_location
+      flash[:danger] = t 'user.notice.danger.not-logged'
+      redirect_to login_url
+    end
+  end
+  
 end
--- app/controllers/users/users_controller.rb
+++ app/controllers/users/users_controller.rb
@@ -60,15 +60,6 @@ class Users::UsersController < ApplicationController
   
   private
   
-  # Confirms a logged-in user.
-  def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = t 'user.notice.danger.not-logged'
-      redirect_to login_url
-    end
-  end
-  
   def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
