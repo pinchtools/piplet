5055b1c59ff676c67e3a195e49f156db609fea9a
vincentp_wm <vincentp@webwag.com>
2016-05-25 18:14:56 +0200

    separate user and general menu and prepare new user controllers

---
+++ app/controllers/users/preferences_controller.rb
@@ -0,0 +1,2 @@
+class Users::PreferencesController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -43,11 +43,13 @@ Rails.application.routes.draw do
     
     resources :users
 
-        
     resources :account_activations, only: [:edit]
     
     resources :password_resets, only: [:new, :create, :edit, :update]
 
+    resources :dashboard, only: [:index]
+    
+    resources :preferences, only: [:index]
   end # namespace users
 
   # The priority is based upon order of creation: first created -> highest priority.
