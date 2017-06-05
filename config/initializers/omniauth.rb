5d1bd473241eee6707d75c6729635f605d7f1786
Vincent <vincent@teezily.com>
2017-06-05 09:21:23 +0200

    fix test migration load

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,17 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
-  provider :google_oauth2, Setting['global.auth']['google']['client_id'], Setting['global.auth']['google']['client_secret'],
-           {
-             name: 'google'
-           }
-  provider :facebook, Setting['global.auth']['facebook']['app_id'], Setting['global.auth']['facebook']['secret_key'],
-           {
-             secure_image_url: true
-           }
+  begin
+    provider :developer unless Rails.env.production?
+    provider :google_oauth2, Setting['global.auth']['google']['client_id'], Setting['global.auth']['google']['client_secret'],
+             {
+               name: 'google'
+             }
+    provider :facebook, Setting['global.auth']['facebook']['app_id'], Setting['global.auth']['facebook']['secret_key'],
+             {
+               secure_image_url: true
+             }
+  rescue ActiveRecord::StatementInvalid
+    # This will happen when migrating a new database
+  rescue => e
+    # Don't stop initialization if something goes wrong here
+  end
 end
