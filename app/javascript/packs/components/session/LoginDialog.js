2859e8aa3ebf87c0c6915c3047618a39051bdbad
Vincent <vincent@teezily.com>
2017-07-24 08:38:56 +0200

    WIP login dialog ui

--- app/javascript/packs/components/session/LoginDialog.js
+++ app/javascript/packs/components/session/LoginDialog.js
@@ -1,24 +1,32 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {FormattedMessage} from 'react-intl'
 import { withStyles, createStyleSheet } from 'material-ui/styles'
 import Dialog, {withResponsiveFullScreen} from 'material-ui/Dialog'
-import List, { ListItem, ListItemText } from 'material-ui/List'
-import Divider from 'material-ui/Divider'
+import Grid from 'material-ui/Grid'
+import Input from 'material-ui/Input'
+import InputLabel from 'material-ui/Input/InputLabel'
+import FormControl from 'material-ui/Form/FormControl'
 import AppBar from 'material-ui/AppBar'
 import Toolbar from 'material-ui/Toolbar'
+import Button from 'material-ui/Button'
 import IconButton from 'material-ui/IconButton'
 import Typography from 'material-ui/Typography'
 import CloseIcon from 'material-ui-icons/Close'
 import Slide from 'material-ui/transitions/Slide'
 
-const styleSheet = createStyleSheet('LoginDialog', {
+
+const styleSheet = createStyleSheet('LoginDialog', theme => ({
   appBar: {
     position: 'relative',
   },
   flex: {
     flex: 1,
   },
-})
+  body: {
+    padding: 10
+  }
+}))
 
 const ResponsiveDialog = withResponsiveFullScreen({breakpoint: 'sm'})(Dialog)
 
@@ -39,15 +47,50 @@ const LoginDialog = ({ classes, loginProps, onLoginToggle }) => {
           </IconButton>
         </Toolbar>
       </AppBar>
-      <List>
-        <ListItem button>
-          <ListItemText primary="Phone ringtone" secondary="Titania" />
-        </ListItem>
-        <Divider />
-        <ListItem button>
-          <ListItemText primary="Default notification ringtone" secondary="Tethys" />
-        </ListItem>
-      </List>
+      <Grid container gutter={24} className={classes.body}>
+        <Grid item xs={12}>
+          <FormControl  fullWidth>
+            <InputLabel htmlFor="username">
+              <FormattedMessage
+                id="LoginDialog.username"
+                defaultMessage={`username`}
+              />
+            </InputLabel>
+            <Input id="username" />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <FormControl fullWidth>
+            <InputLabel htmlFor="password">
+              <FormattedMessage
+                id="LoginDialog.password"
+                defaultMessage={`password`}
+              />
+            </InputLabel>
+            <Input id="password" type="password" />
+          </FormControl>
+        </Grid>
+        <Grid item xs={12}>
+          <Grid container justify="flex-end">
+            <Grid item >
+              <Button onClick={() => onLoginToggle()}>
+                <FormattedMessage
+                  id="LoginDialog.cancel"
+                  defaultMessage={`cancel`}
+                />
+              </Button>
+            </Grid>
+            <Grid item >
+              <Button raised color='accent' onClick={() => onLoginToggle()}>
+                <FormattedMessage
+                  id="LoginDialog.submit"
+                  defaultMessage={`submit`}
+                />
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
     </ResponsiveDialog>
   )
 }
