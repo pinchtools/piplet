320a5e16ab2383ce92b32240dbce33eb46623b29
Vincent <vincent@teezily.com>
2017-09-09 17:02:30 +0200

    simplify email validation rule

--- lib/validators/email_validator.rb
+++ lib/validators/email_validator.rb
@@ -8,30 +8,30 @@ class EmailValidator < ActiveModel::EachValidator
 
   def validate_each(record, attribute, value)
     return if value.blank?
-    
+
     self.record = record
     self.attribute = attribute
     self.value = value
-    
+
     is_using_banned_provider?
     well_formated?
     not_too_long?
   end
 
   def well_formated?
-    record.errors.add(attribute, :invalid) unless !!(value =~ /\A[^@\s]+@[^\@\s\.]+\.[^\@\s]+[^\@\.\s]+\z/)
+    record.errors.add(attribute, :invalid) unless value.include?('@')
   end
 
   def not_too_long?
     record.errors.add(attribute, :too_long, count: MAX_SIZE) if value.length > MAX_SIZE
   end
-  
+
   def is_using_banned_provider?
     return unless record.errors.empty?
-    
+
     if BannedEmailProviders.include?(value)
       record.errors.add(attribute, I18n.t(:'user.errors.email.provider-banned'))
     end
   end
-  
+
 end
