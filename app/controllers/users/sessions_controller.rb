1087979df9133d2b1fd3aee2f682ee63c2c6bdfd
Vincent <vincent@teezily.com>
2017-10-03 08:27:39 +0200

    add concernedfilter call on create + refacto test

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,33 +1,40 @@
 class Users::SessionsController < ApplicationController
   def new
   end
-  
+
   def create
     user = User.all_valid.find_by(email: params[:session][:email].downcase)
 
     if user && user.authenticate(params[:session][:password])
-      if user.activated?
-        log_in user
-        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-          
-        if user.regular?
-          redirect_back_or users_dashboard_index_path
-        else
-          redirect_back_or admin_dashboard_index_path
-        end
-      else
+      if !user.activated?
         flash[:warning] = t('user.notice.warning.account-not-activated')
-        redirect_to root_url
+        redirect_to root_url and return
+      elsif Users::ConcernedByFiltersService.new(user).call
+        user.errors.add(:base, I18n.t('user.notice.danger.invalid-login'))
       end
     else
-      flash.now[:danger] = t('user.notice.danger.invalid-login')
-      render 'new'
+      user ||= User.new
+      user.errors.add(:base, I18n.t('user.notice.danger.invalid-login'))
+    end
+
+    if user.errors.empty?
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+
+      if user.regular?
+        redirect_back_or users_dashboard_index_path
+      else
+        redirect_back_or admin_dashboard_index_path
+      end
+    else
+      flash.now[:danger] = user.errors.full_messages.first
+      render 'new', status: :unprocessable_entity
     end
   end
-  
+
   def destroy
     log_out if logged_in?
     redirect_to root_url
   end
-  
+
 end
